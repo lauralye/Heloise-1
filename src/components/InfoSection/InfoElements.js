33d85c2641802b969f5370b511d8a45220d5c3ea
LauraLye <jiaying@maxis.com.my>
2022-04-10 12:39:39 +0800

    Created landing page

---
+++ src/components/InfoSection/InfoElements.js
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+
+
+export const InfoContainer = styled.div`
+
+    color:#fff;
+    background: ${({lightBg}) => (lightBg ? '#0f9f9f9' : '#010606')};
+
+
+    @media scren and (max-width: 768px){
+
+        padding: 100px 0;
+
+    }
+    
+`
+
+export const InfoWrapper = styled.div`
+    display: grid;
+    z-index: 1;
+    height: 860px;
+    width: 100%
+    max-width: 1100px;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 0 24px;
+    justify-content: center;
+
+
+
+`
+
+export const InfoRow = styled.div`
+    display: grid;
+    grid-auto-column:
+    align-items:
+    grid-template-areas:
+
+
+
+`
